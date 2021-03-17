17a74b866a811467114e2efff756b3722184f175
hassan556 <hassanm19@gmail.com>
2021-03-17 07:53:14 +0000

    Disable systempart for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8952
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=tty0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk ramoops_memreserve=4M androidboot.selinux=permissive
-BOARD_KERNEL_CMDLINE += systempart=/dev/mmcblk0p24
+#BOARD_KERNEL_CMDLINE += systempart=/dev/mmcblk0p24
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
